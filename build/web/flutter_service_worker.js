665af2ec4618eea570fbdb198eb2d658191732a4
unknown <mafianawaz123@gmail.com>
2025-11-06 16:01:27 +0500

    Complete styling

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -53,7 +53,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9ce2f39210702a5fcf86fb84807ffcfe
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "581e8068106ce29cd31b6a4bdf550f36",
+"flutter_bootstrap.js": "e8f3940235fe2cd9c163be1ed49a2d60",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
